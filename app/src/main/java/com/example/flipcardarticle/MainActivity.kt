80ed9e0aec90aa1b2cd061686bbdcbef0a89ce5f
Aleksandr Masliaev <aleksandr.masliaev@dif.tech>
2023-05-17 14:41:15 +0700

    initial commit

---
+++ app/src/main/java/com/example/flipcardarticle/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.flipcardarticle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.flipcardarticle.ui.theme.FlipCardArticleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FlipCardArticleTheme {}
+        }
+    }
+}
